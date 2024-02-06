1d7973f07f13a03907e824eecb1a2218a3131722
Mikael Kristensen <mikael@hvitfjell.no>
2024-02-06 19:21:31 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 */
 
 const inputbtn = document.getElementById("input-btn")
+const convertBtn = document.getElementById("convert-btn")
 const lengthField = document.getElementById("length-field")
 const volumeField = document.getElementById("volume-field")
 const massField = document.getElementById("mass-field")
@@ -19,18 +20,28 @@ inputbtn.addEventListener("keypress", function (e) {
     }
 })
 
+convertBtn.addEventListener("click", function () {
+    render(getInput)
+})
+
 function getInput() {
     let input = Number(inputbtn.value)
-    return input;
+    console.log(input)
 }
 
 function convertMetrics(input, metric) {
     switch (metric) {
         case 'length':
-            return (input)
+            return `${input} meter = ${(input * meterToFeet).toFixed(3)} feet = ${(input / meterToFeet).toFixed(3)}`
+        case 'volume':
+            return `${input} liters = ${(input * literToGallon).toFixed(3)} | ${input} feet = ${(input / literToGallon).toFixed(3)}`
+        case 'mass':
+            return `${input} meter = ${(input * kilogramToPound).toFixed(3)} | ${input} feet = ${(input / kilogramToPound).toFixed(3)}`
+            defult:
+            throw new Error("Invalid target metric")
     }
 }
 
-function render(conversion) {
+function render() {
 
 }
